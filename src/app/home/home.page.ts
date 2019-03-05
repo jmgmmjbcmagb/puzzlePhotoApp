123304f329da74d6dfae180afba8de391a9673e7
Jesús María Gómez Márquez <jesusmariagomezmarquez@gmail.com>
2019-03-05 01:24:26 +0100

    feat: put more photos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, CdkDropList, transferArrayItem, CdkDrag, moveItemInArray } from '@angular/cdk/drag-drop';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -7,17 +9,75 @@ import { CdkDragDrop, CdkDropList, transferArrayItem, CdkDrag, moveItemInArray }
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public alertController: AlertController) { }
+
+
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Hecho!',
+      message: 'Has completado el puzzle',
+      buttons: [
+        {
+          text: '👍',
+          handler: () => {
+            this.reload();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  img = 'https://images.pexels.com/photos/163077/mario-yoschi-figures-funny-163077.jpeg'
+
+  imgs = [
+    {
+      img: "https://images.pexels.com/photos/163077/mario-yoschi-figures-funny-163077.jpeg"
+    },
+    {
+      img: "https://vignette.wikia.nocookie.net/regularshow/images/e/e9/Shimmer-and-shine-2-2-1x1.jpg/revision/latest?cb=20180217163036&path-prefix=es"
+    },
+    {
+      img: "http://s3.amazonaws.com/nickelodeonparents.com-production/wp-content/uploads/2016/03/shimmerAndShine-pinThePonytail1x1.jpg"
+    },
+    {
+      img: "https://stmed.net/sites/default/files/peppa-pig-wallpapers-26290-4958877.jpg"
+    },
+    {
+      img: "https://theshirleyjourney.com/wp-content/uploads/2015/07/US-Peppa-Sandcastles.jpg"
+    },
+    {
+      img: "https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/peliculas-para-ninos-cine-infantil/trailer-pelicula-pocoyo/138090472-1-esl-ES/Pocoyo-en-cines-Trailer-de-la-pelicula.jpg"
+    },
+    {
+      img: "https://pbs.twimg.com/media/DBePz0FUAAAAas-.jpg"
+    }
+  ]
+
+  changeImg(img) {
+    this.reload();
+    this.img = img;
+  }
+
   todo = [
-    { value: '1', done: 'done1', img: 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-EykWMbkO0cNr4Pie/image_part_001.jpg' },
-    { value: '2', done: 'done2', img: 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-EykWMbkO0cNr4Pie/image_part_002.jpg' },
-    { value: '3', done: 'done3', img: 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-EykWMbkO0cNr4Pie/image_part_003.jpg' },
-    { value: '4', done: 'done4', img: 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-EykWMbkO0cNr4Pie/image_part_004.jpg' }
+    { value: '1', done: 'done1' },
+    { value: '2', done: 'done2' },
+    { value: '3', done: 'done3' },
+    { value: '4', done: 'done4' }
   ];
 
   done1 = [];
   done2 = [];
   done3 = [];
   done4 = [];
+
+  slideOpts = {
+    effect: 'flip',
+    spaceBetween: 25,
+    slidesPerView: 3
+  };
 
   evenPredicate(drag: CdkDrag, drop: CdkDropList) {
     if (drag.data.done === drop.id) {
@@ -27,15 +87,13 @@ export class HomePage {
     }
   }
 
-  public reload(): void{
-    console.log("console")
+  public reload(): void {
     this.todo = [
-      { value: '1', done: 'done1', img: 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-EykWMbkO0cNr4Pie/image_part_001.jpg' },
-      { value: '2', done: 'done2', img: 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-EykWMbkO0cNr4Pie/image_part_002.jpg' },
-      { value: '3', done: 'done3', img: 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-EykWMbkO0cNr4Pie/image_part_003.jpg' },
-      { value: '4', done: 'done4', img: 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-EykWMbkO0cNr4Pie/image_part_004.jpg' }
+      { value: '1', done: 'done1' },
+      { value: '2', done: 'done2' },
+      { value: '3', done: 'done3' },
+      { value: '4', done: 'done4' }
     ];
-  
     this.done1 = [];
     this.done2 = [];
     this.done3 = [];
@@ -50,7 +108,10 @@ export class HomePage {
         event.container.data,
         event.previousIndex,
         event.currentIndex);
-        event.container.disabled = true
+      event.container.disabled = true
+    }
+    if (event.previousContainer.data.length === 0) {
+      this.presentAlertConfirm();
     }
   }
 }
